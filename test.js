257abf347f962be7c01173e6e55deb25741bd464
Matthew Crumley <email@matthewcrumley.com>
2017-10-22 20:36:59 -0400

    Add a maximum password length option
    Fixes #6

--- test.js
+++ test.js
@@ -1,11 +1,12 @@
 // very naive testing
 
 var assert = require("assert"),
-    easyPbkdf2 = require("./easy-pbkdf2")( {"DEFAULT_HASH_ITERATIONS": 256, "SALT_SIZE": 128, "KEY_LENGTH": 1024} );
+    easyPbkdf2 = require("./easy-pbkdf2")( {"DEFAULT_HASH_ITERATIONS": 256, "SALT_SIZE": 128, "KEY_LENGTH": 1024, "MAX_PASSWORD_LENGTH": 2048} );
 
 assert.strictEqual( easyPbkdf2.DEFAULT_HASH_ITERATIONS, 256, "DEFAULT_HASH_ITERATIONS set correctly");
 assert.strictEqual( easyPbkdf2.SALT_SIZE, 128, "SALT_SIZE set correctly");
 assert.strictEqual( easyPbkdf2.KEY_LENGTH, 1024, "KEY_LENGTH set correctly");
+assert.strictEqual( easyPbkdf2.MAX_PASSWORD_LENGTH, 2048, "MAX_PASSWORD_LENGTH set correctly");
 
 var salt = easyPbkdf2.generateSalt();
 assert.ok( salt && salt.length > 0, "Sync salt created");
@@ -63,7 +64,33 @@ easyPbkdf2.generateSalt( function( salt ){
                 easyPbkdf2.random( 10, function( randomBytes ){
                     assert.equal( randomBytes.length, 10, "Async random returns correct randomBytes" );
 
-                    console.log("done. all tests passes");
+                    easyPbkdf2.MAX_PASSWORD_LENGTH = 7
+                    easyPbkdf2.secureHash( "password", "salt", function(err){
+                        assert.ok( err instanceof Error, "Hashing a too-long password emits error" );
+                        assert.equal( err.message, "Password exceeds maximum length of 7", "Hashing a too-long password has correct error message" );
+
+                        easyPbkdf2.MAX_PASSWORD_LENGTH = 8
+                        easyPbkdf2.secureHash( "password", "salt", function(err){
+                            assert.ok( !err, "No error when password length is at the limit" );
+                            assert.ok( hashed && hashed.length > 0, "Hash created when password is at the length limit");
+                            assert.ok( salt && salt.length > 0, "Salt created when password is at the length limit");
+
+                            easyPbkdf2.MAX_PASSWORD_LENGTH = 7
+                            easyPbkdf2.verify( salt, hashed, password, function( err, valid ) {
+                                assert.ok( err instanceof Error, "Verifying a too-long password emits error" );
+                                assert.equal( err.message, "Password exceeds maximum length of 7", "Verifying a too-long password has correct error message" );
+                                assert.ok( !valid, "Verify returns correct result for too-long password");
+
+                                easyPbkdf2.MAX_PASSWORD_LENGTH = 8
+                                easyPbkdf2.verify( salt, hashed, password, function( err, valid ) {
+                                    assert.ok( !err, "No error when verifying password at the limit" );
+                                    assert.ok( valid, "Verify returns correct result for password at the length limit");
+
+                                    console.log("done. all tests passes");
+                                });
+                            });
+                        });
+                    });
                 });
             });
 
